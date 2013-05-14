0ad335b5a860515d95e7689d566ca08b4e2f178c
Callum <callum.terris@gmail.com>
2013-05-14 19:34:53 +0100

    Caved and stopped trying wcf and I am now using just asmx. Nothing much different. I have now got it working with the wander command. Going to seperate all the prototypes into their own folders. Next is to create the neural network. Going to create that then worry about adding in the networking part. Also need to look at hosting two servers on the same machine. Just will be a case of changing the local host port i guess.
    Signed-off-by: Callum <callum.terris@gmail.com>

--- Unity Masters/Assets/Wander.cs
+++ Unity Masters/Assets/Wander.cs
@@ -1,21 +1,25 @@
 using UnityEngine;
 using System.Collections;
 
+
 public class Wander : MonoBehaviour {
 	
 	int speed =10;
 	Vector3 direction;
-	Random rand;
+	Random rand = new Random();
 	int randomDirection;
+	MyService service;
 	
 	// Use this for initialization
 	void Start () {
-	
+	service = new MyService();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		randomDirection = Random.Range(-10,10);	
+		randomDirection = service.Random(rand,10,-10);
+		//randomDirection = Random.Range(-10,10);	
+		
 		//direction.x = direction.x + randomDirection;
 		transform.Rotate(new Vector3(0, direction.y + randomDirection,0));
 		transform.Translate(Vector3.forward * (Time.deltaTime* speed));
