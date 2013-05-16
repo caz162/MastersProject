8414dccd4e0d440a082cb6b5b142ce53aa6b662c
Callum <callum.terris@gmail.com>
2013-05-16 13:42:49 +0100

    Started writing the NN in Visual studio. Have the basic network down. Going to try and connect this to the interface. Will need to create a server for this first. Created the prototype 4 folder and copied files from proto 3 into it.
    Need to draw diagrams of the network and how it is going to work. Also need to stress test the network
    
    Signed-off-by: Callum <callum.terris@gmail.com>

---
+++ Visual Studio NN/Visual Studio NN/Neuron.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Visual_Studio_NN
+{
+    public enum NeuronPlace  {input, bias, hidden, output};
+
+    class Neuron
+    {
+        float inputNum;
+        List<Connection> inputConnections = new List<Connection>();
+        List<Connection> outputConnections = new List<Connection>();
+        NeuronPlace place;
+        int counter = 0;
+
+        public void SetPlace(NeuronPlace p)
+        {
+            place = p;
+        }
+
+        public void AddInputConnection(Connection c)
+        {
+            inputConnections.Add(c);
+        }
+
+        public void AddOutputConnection(Connection c)
+        {
+            outputConnections.Add(c);
+        }
+
+        void SendData(float num)
+        {
+            foreach (Connection c in outputConnections)
+            {
+                c.PassData(num);
+            }
+        }
+
+        void Activation()
+        {
+            if (inputNum >= 1)
+            {
+                SendData(1);
+            }
+            else
+            {
+                SendData(0);
+            }
+        }
+
+        public void RecieveData(float num)
+        {
+            counter++;
+            if (place == NeuronPlace.input){
+                if (counter > inputConnections.Count+1)
+                {
+                    inputNum += num;
+                    SendData(inputNum);
+                }
+                else
+                {
+                    inputNum += num;
+                   // counter++;
+                }
+
+            }
+            else
+            if (counter >= inputConnections.Count)
+            {
+                inputNum += num;
+                SendData(inputNum);
+            }
+            else
+            {
+                inputNum += num;
+                //counter++;
+            }
+        }
+    }
+}
